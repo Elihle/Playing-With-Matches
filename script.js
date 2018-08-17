a141818932fb51daa4264677cb48f130e2a22c84
Lihle <lihlemenz@gmail.com>
2018-08-17 14:24:42 +0200

    fixed errors on script file

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-function getRandom() {
-    return Math.ceil(Math.random() * 9);
-}
+// function getRandom() {
+//     return Math.ceil(Math.random() * 9);
+// }
+
+// function showNumber(number){
+//     let number = document.querySelector('.number');
+//     for (let i = 0; i < number.length; i++) {
+//           number = number[i];
+//         }
+// }
+//     number.innerHTML = getRandom();
 
 
-function showNumber(number){
-    let number = document.querySelector('.number');
-    for (let i = 0; i < number; i++) {
-        if ((number[i])) {
-          number = number[i];
-        }
-        return
-}
-}
-number.innerHTML = getRandom();
+    function getRandom() {
+        return Math.ceil(Math.random() * 9);
+    }
+    
+    // let number = document.querySelector('.number');
+    // let numOne = document.querySelector('.numOne');
+    // let numTwo = document.querySelector('.numTwo');
+    // let numThree = document.querySelector('.numThree');
 
+    var elems  = document.querySelectorAll('.number') // = [number, numOne, numTwo];
 
-function showNumber
+    for (let index = 0; index < elems.length; index++) {
+        const number = elems[index];
+        number.innerHTML = getRandom();   
+    }
+
+    // number.innerHTML = getRandom(); 
+    // numOne.innerHTML = getRandom();
+    // numTwo.innerHTML = getRandom();
+    
